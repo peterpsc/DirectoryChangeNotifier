d231411b5ce78cd48cfabe80ebe23bf085e9d00f
Peter Carmichael <peter.carmichael@comcast.net>
2026-02-27 07:55:52 -0500

    fix DirChangeNotifier.bat

--- DirChangeNotifier.bat
+++ DirChangeNotifier.bat
@@ -2,7 +2,7 @@ cd C:\Users\peter\PycharmProjects\DirectoryChangeNotifier
 git pull
 xcopy A:\SourceCode\PycharmProjects\DirectoryChangeNotifier\Private\*.* .\Private /D /c /i /h /k /y /EXCLUDE:backup.exclude
 
-python.exe DirChangeNotifier.py
+py DirChangeNotifier.py
 
 xcopy .\Private\*.* A:\SourceCode\PycharmProjects\DirectoryChangeNotifier\Private\ /D /c /i /h /k /y /EXCLUDE:backup.exclude
 pause
